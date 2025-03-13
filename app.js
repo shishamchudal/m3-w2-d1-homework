16bb417e4a0373332c5e68508fd86fbcd69e8b72
Shisham Chudal <shishamchudal61@gmail.com>
2025-03-13 14:51:16 -0700

    Integrate MongoDB connection and create collection for city statistics

--- app.js
+++ app.js
@@ -1,44 +1,61 @@
-var stats =[
-    {
-        'city': 'San Juan', 
-        'zip': '00926', 
-        'state': 'PR', 
-        'income': '34781',
-        'age': '44'
-    },
-    {
-        'city': 'Corona', 
-        'zip': '11368', 
-        'state': 'NY', 
-        'income': '50797',
-        'age': '32'
-    },
-    {
-        'city': 'Chicago', 
-        'zip': '60629', 
-        'state': 'IL', 
-        'income': '42019',
-        'age': '31'
-    },
-    {
-        'city': 'El Paso', 
-        'zip': '79936', 
-        'state': 'TX', 
-        'income': '54692',
-        'age': '31'
-    },
-    {
-        'city': 'Los Angeles', 
-        'zip': '90011', 
-        'state': 'CA', 
-        'income': '36954',
-        'age': '28'
-    },
-    {
-        'city': 'Norwalk', 
-        'zip': '90650', 
-        'state': 'CA', 
-        'income': '66453',
-        'age': '35'
-    }
-]
+const { MongoClient } = require("mongodb");
+
+// Create Instance of MongoClient for mongodb
+const client = new MongoClient("mongodb://127.0.0.1:27017/nodemongo");
+
+client
+  .connect()
+  .then(() => {
+    var dbo = client.db("statsdb");
+
+    dbo.createCollection("uscensus").then(function () {
+        //   console.log("Collection created");
+        //   //Close the database connection
+        //   client.close();
+        // });
+    var stats = [
+      {
+        city: "San Juan",
+        zip: "00926",
+        state: "PR",
+        income: "34781",
+        age: "44",
+      },
+      {
+        city: "Corona",
+        zip: "11368",
+        state: "NY",
+        income: "50797",
+        age: "32",
+      },
+      {
+        city: "Chicago",
+        zip: "60629",
+        state: "IL",
+        income: "42019",
+        age: "31",
+      },
+      {
+        city: "El Paso",
+        zip: "79936",
+        state: "TX",
+        income: "54692",
+        age: "31",
+      },
+      {
+        city: "Los Angeles",
+        zip: "90011",
+        state: "CA",
+        income: "36954",
+        age: "28",
+      },
+      {
+        city: "Norwalk",
+        zip: "90650",
+        state: "CA",
+        income: "66453",
+        age: "35",
+      },
+    ];
+  })
+  .catch((error) => console.log("Failed to connect", error));
