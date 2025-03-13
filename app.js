6b5e3c90ce34c6cd6a634e72e584a207a76acde3
Shisham Chudal <shishamchudal61@gmail.com>
2025-03-13 14:57:24 -0700

    Update city statistics dataset with new entries for Pacoima and Ketchikan

--- app.js
+++ app.js
@@ -13,48 +13,73 @@ client
     //   //Close the database connection
     //   client.close();
     // });
+    // var stats = [
+    //   {
+    //     city: "San Juan",
+    //     zip: "00926",
+    //     state: "PR",
+    //     income: "34781",
+    //     age: "44",
+    //   },
+    //   {
+    //     city: "Corona",
+    //     zip: "11368",
+    //     state: "NY",
+    //     income: "50797",
+    //     age: "32",
+    //   },
+    //   {
+    //     city: "Chicago",
+    //     zip: "60629",
+    //     state: "IL",
+    //     income: "42019",
+    //     age: "31",
+    //   },
+    //   {
+    //     city: "El Paso",
+    //     zip: "79936",
+    //     state: "TX",
+    //     income: "54692",
+    //     age: "31",
+    //   },
+    //   {
+    //     city: "Los Angeles",
+    //     zip: "90011",
+    //     state: "CA",
+    //     income: "36954",
+    //     age: "28",
+    //   },
+    //   {
+    //     city: "Norwalk",
+    //     zip: "90650",
+    //     state: "CA",
+    //     income: "66453",
+    //     age: "35",
+    //   },
+    // ];
+
+    // dbo
+    //   .collection("uscensus")
+    //   .insertMany(stats)
+    //   .then(function (res) {
+    //     console.log("Number of documents inserted:" + res.insertedCount);
+    //     client.close();
+    //   });
+
     var stats = [
       {
-        city: "San Juan",
-        zip: "00926",
-        state: "PR",
-        income: "34781",
-        age: "44",
-      },
-      {
-        city: "Corona",
-        zip: "11368",
-        state: "NY",
-        income: "50797",
-        age: "32",
-      },
-      {
-        city: "Chicago",
-        zip: "60629",
-        state: "IL",
-        income: "42019",
-        age: "31",
-      },
-      {
-        city: "El Paso",
-        zip: "79936",
-        state: "TX",
-        income: "54692",
-        age: "31",
-      },
-      {
-        city: "Los Angeles",
-        zip: "90011",
+        city: "Pacoima",
+        zip: "91331",
         state: "CA",
-        income: "36954",
-        age: "28",
+        income: "60360",
+        age: "33",
       },
       {
-        city: "Norwalk",
-        zip: "90650",
-        state: "CA",
-        income: "66453",
-        age: "35",
+        city: "Ketchikan",
+        zip: "99950",
+        state: "AK",
+        income: "00000",
+        age: "00",
       },
     ];
 
